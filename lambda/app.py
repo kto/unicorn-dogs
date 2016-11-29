fa4ce2e36e11263ada33dfb2dfe62de491a1b527
Kimmo Toro <kimmo.toro@f-secure.com>
2016-11-29 03:04:51 +0200

    Start to use common message processor

--- lambda/app.py
+++ lambda/app.py
@@ -1,10 +1,11 @@
 import urllib2
 import boto3
 import json
+import logging
 
-MESSAGES = {}
 API_BASE = 'https://dashboard.cash4code.net/score'
 API_TOKEN = 'bb311aeada'
+handler = Handler(API_TOKEN, API_BASE, logging.getLogger())
 
 s3_client = boto3.client('s3')
 
@@ -14,46 +15,5 @@ def lambda_handler(event, context):
         key = record['s3']['object']['key']
         msg_data = s3_client.get_object(Bucket=bucket, Key=key)
         if 'Body' in msg_data:
-            handle_event(json.load(msg_data['Body']))
-
-
-def handle_event(msg):
-    msg_id = msg['Id']  # The unique ID for this message
-    part_number = int(msg['PartNumber'])  # Which part of the message it is
-    total_parts = int(msg['TotalParts'])  # How many
-    data = msg['Data']  # The data of the message
-
-    # Try to get the parts of the message from the MESSAGES dictionary.
-    # If it's not there, create one that has None in both parts
-    msg = MESSAGES.get(msg_id, dict(sent=False, parts=[None] * total_parts))
-
-    # store this part of the message in the correct part of the list
-    msg['parts'][part_number] = data
-
-    # store the parts in MESSAGES
-    MESSAGES[msg_id] = msg
-
-    # if both parts are filled, the message is complete
-    if None not in msg['parts']:
-        if msg['sent']:
-            print "not sending again"
-            return 'OK'
-        print "have all parts"
-        # We can build the final message.
-        result = ''.join(msg['parts'])
-        # sending the response to the score calculator
-        # format:
-        #   url -> api_base/jFgwN4GvTB1D2QiQsQ8GHwQUbbIJBS6r7ko9RVthXCJqAiobMsLRmsuwZRQTlOEW
-        #   headers -> x-gameday-token = API_token
-        #   data -> EaXA2G8cVTj1LGuRgv8ZhaGMLpJN2IKBwC5eYzAPNlJwkN4Qu1DIaI3H1zyUdf1H5NITR
-        url = API_BASE + '/' + msg_id
-        print url
-        print result
-        req = urllib2.Request(url, data=result, headers={'x-gameday-token':ARGS.API_token})
-        resp = urllib2.urlopen(req)
-        resp.close()
-        msg['sent'] = True
-        print resp
-
-    return 'OK'
+            return handler.process_message(json.load(msg_data['Body']))
 
---
+++ lambda/handler.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+"""
+Client which receives the requests
+
+Args:
+    API Token
+    API Base (https://...)
+
+"""
+import urllib2
+import redis
+import os
+
+#logging.basicConfig(level=logging.DEBUG)
+
+# defining global vars
+
+CACHE_ENDPOINT = os.environ.get('REDIS_ENDPOINT', 'unicorn-dogs.nmwbnf.ng.0001.euc1.cache.amazonaws.com')
+
+
+class Handler(object):
+
+
+    def __init__(self, api_token, api_base, logger):
+        self.MESSAGES = {}
+        self.logger = logger
+        self.redis_client = None
+        self.api_token = api_token
+        self.api_base = api_base
+
+        if CACHE_ENDPOINT is not None:
+            self.redis_client = redis.StrictRedis(host=CACHE_ENDPOINT, port=6379, db=0)
+
+    def process_message(self, data):
+        """
+        processes the messages by combining and appending the kind code
+        """
+        msg_id = data['Id']  # The unique ID for this message
+        part_number = int(data['PartNumber'])  # Which part of the message it is
+        total_parts = int(data['TotalParts'])  # How many
+        raw = data['Data']  # The data of the message
+
+        # Try to get the parts of the message from the MESSAGES dictionary.
+        # If it's not there, create one that has None in both parts
+        #msg = self.redis_client.get(msg_id)
+        #print "redis data", msg
+        msg = self.MESSAGES.get(msg_id, dict(sent=False, parts=[None] * total_parts))
+        print "received", msg_id, part_number, raw
+
+        # store this part of the message in the correct part of the list
+        msg['parts'][part_number] = raw
+
+        # store the parts in MESSAGES
+        self.MESSAGES[msg_id] = msg
+        #self.redis_client.set(msg_id, msg)
+
+        # if both parts are filled, the message is complete
+        if None not in msg['parts']:
+            if msg['sent']:
+                print "not sending again"
+                return 'OK'
+            # app.logger.debug("got a complete message for %s" % msg_id)
+            print "have all parts, sending response to", self.api_base
+            # We can build the final message.
+            result = ''.join(msg['parts'])
+            # sending the response to the score calculator
+            # format:
+            #   url -> api_base/jFgwN4GvTB1D2QiQsQ8GHwQUbbIJBS6r7ko9RVthXCJqAiobMsLRmsuwZRQTlOEW
+            #   headers -> x-gameday-token = API_token
+            #   data -> EaXA2G8cVTj1LGuRgv8ZhaGMLpJN2IKBwC5eYzAPNlJwkN4Qu1DIaI3H1zyUdf1H5NITR
+            self.logger.debug("ID: %s" % msg_id)
+            self.logger.debug("RESULT: %s" % result)
+            url = self.api_base + '/' + msg_id
+            req = urllib2.Request(url, data=result, headers={'x-gameday-token': self.api_token})
+            resp = urllib2.urlopen(req)
+            resp.close()
+            msg['sent'] = True
+            #self.redis_client.set(msg_id, msg)
+
+        return 'OK'
